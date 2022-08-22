c9607afeb722880a30a56ebdd6d41eed4be0414d
denmark-dangangui <dlrgks88@naver.com>
2022-08-22 23:58:44 +0900

    java 배운것 2022 08 22

---
+++ Data_and_operation/StringOperation.java
@@ -0,0 +1,10 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world".length());
+		System.out.println("Hello, leezche ... bye. ".replace("leezche", "leetaemin"));
+	}
+
+}
