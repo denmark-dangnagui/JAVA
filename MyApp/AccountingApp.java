e6efda1c6a86384e7acb70f785355c87c0b9b53f
denmark-dangangui <dlrgks88@naver.com>
2022-08-24 23:03:15 +0900

    Update AccountingApp.java

--- MyApp/AccountingApp.java
+++ MyApp/AccountingApp.java
@@ -3,14 +3,27 @@ public class AccountingApp {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Value of supply : " + 12345.0);
-		System.out.println("VAT : " + (12345.0 * 0.1) );
-		System.out.println("Total : " + (12345.0 + 12345.0 * 0.1) );
-		System.out.println("Expense : " + (12345.0 * 0.3) );
-		System.out.println("Income : " + (12345.0 - 12345.0 * 0.3) );
-		System.out.println("Dividend 1 : " + (12345.0 - 12345.0 * 0.3) *0.5 );
-		System.out.println("Dividend 2 : " + (12345.0 - 12345.0 * 0.3) *0.3 );
-		System.out.println("Dividend 3 : " + (12345.0 - 12345.0 * 0.3) *0.2 );
+		
+		// string을 double로 바꾸는 문법 -> Double.parsedouble(string);
+		double valueOfSupply =Double.parseDouble(args[0]);
+		double VATrate = 0.1;
+		double expenseRate = 0.3;
+		double VAT = valueOfSupply * VATrate;
+		double Total = valueOfSupply + VAT;
+		double Expense = valueOfSupply * expenseRate;
+		double Income = valueOfSupply - Expense;
+		double dividend1 = Income *0.5;
+		double dividend2 = Income *0.3;
+		double dividend3 = Income *0.2;
+
+		System.out.println("Value of supply : " + valueOfSupply);
+		System.out.println("VAT : " + VAT );
+		System.out.println("Total : " + Total );
+		System.out.println("Expense : " + Expense );
+		System.out.println("Income : " + Income );
+		System.out.println("Dividend 1 : " + dividend1 );
+		System.out.println("Dividend 2 : " + dividend2 );
+		System.out.println("Dividend 3 : " + dividend3 );
 		
 		
 	}
