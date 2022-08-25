42e24727b674e6da1b7fcc5ff81f6c36d1a4d7b9
denmark-dangangui <dlrgks88@naver.com>
2022-08-25 23:55:43 +0900

    new exception start

---
+++ JavaFlowControl/ArrayApp.java
@@ -0,0 +1,20 @@
+
+public class ArrayApp {
+
+	public static void main(String[] args) {
+//		String	users = ""
+		String[] users = new String[3];
+		users[0] ="egoing";
+		users[1] ="jinhuck";
+		users[2] ="yubin";
+		
+		System.out.println(users.length);
+		
+		
+		int[] scores = {10,100,100};
+		System.out.println(scores[1]);
+		
+		
+	}
+
+}
