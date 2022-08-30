3146eeee9e22a282cfd32ea9b8531dc0ac3ec834
denmark-dangangui <dlrgks88@naver.com>
2022-08-31 00:27:01 +0900

    this update

--- ProjectOOP/print.java
+++ ProjectOOP/print.java
@@ -1,18 +1,20 @@
 
 class print{
 	public String delimiter = "";	
-	public print(String _delimiter) {
-		delimiter = _delimiter;
+	public print(String delimiter) {
+		this.delimiter = delimiter;
 	}
 	public void A() {
-		System.out.println(delimiter);
+		System.out.println(this.delimiter);
 		System.out.println("A");
 		System.out.println("A");
 	}
 	
 	public void B() {
-		System.out.println(delimiter);
+		System.out.println(this.delimiter);
 		System.out.println("B");
 		System.out.println("B");
 	}
 }
+
+// this는 클래스가 인스턴스화 되었을 때 instance 자체를 가리키는 특수한 단어이다. 
