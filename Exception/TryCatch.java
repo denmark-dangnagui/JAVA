15efac28ae10b69db9a9a15cc29f9db258478459
denmark-dangangui <dlrgks88@naver.com>
2022-08-30 10:54:54 +0900

    IOException / finally / close

--- Exception/TryCatch.java
+++ Exception/TryCatch.java
@@ -6,11 +6,13 @@ public class TryCatch {
 		int [] scores = {10,20,30};
 		
 		try {
-//			System.out.println(scores[4]);
+			System.out.println(scores[4]);
 			System.out.println(2/0);
-		} catch (Exception e) {
+		} catch (ArithmeticException e) {
+			System.out.println("계산이 잘못됐습니다.");
+		}catch (Exception e) {
 			System.out.println("먼가 이상합니다. 오류가 발생했습니다.");
-		}
+		} 
 		System.out.println(3);
 	}
 }
