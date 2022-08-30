a761614c9fdc24d7e6998454b2041b4160d5eb8a
denmark-dangangui <dlrgks88@naver.com>
2022-08-30 10:23:12 +0900

    trycatch_2 (Exception)
    포괄적으로 사용하는 Exception

--- Exception/TryCatch.java
+++ Exception/TryCatch.java
@@ -4,17 +4,14 @@ public class TryCatch {
 	public static void main(String[] args) {
 		System.out.println(1);
 		int [] scores = {10,20,30};
+		
 		try {
-			System.out.println(scores[4]);
-		} catch(ArrayIndexOutOfBoundsException e) {
-			System.out.println("잘못된 인덱스네요.");
-		}
-		try {
+//			System.out.println(scores[4]);
 			System.out.println(2/0);
-		} catch (ArithmeticException e) {
-			System.out.println("잘못된 계산이네요.");
+		} catch (Exception e) {
+			System.out.println("먼가 이상합니다. 오류가 발생했습니다.");
 		}
 		System.out.println(3);
 	}
-
 }
+    
