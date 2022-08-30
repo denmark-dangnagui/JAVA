8fbd33da23d2a6c548b1b573af2cab39d37941b8
denmark-dangangui <dlrgks88@naver.com>
2022-08-30 23:26:15 +0900

    Class

---
+++ ProjectOOP/MyOOP.java
@@ -0,0 +1,16 @@
+public class MyOOP {	
+	public static void main(String[] args) {
+		print.delimiter = "----";
+		print.A();
+		print.A();
+		print.B();
+		print.B();
+
+		print.delimiter = "****";
+		print.A();
+		print.A();
+		print.B();
+		print.B();
+	}
+
+}
