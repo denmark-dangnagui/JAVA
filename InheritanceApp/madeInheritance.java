442d60381637bb6d96b1c29469c63b9703d5b2e2
denmark-dangangui <dlrgks88@naver.com>
2022-08-31 17:13:42 +0900

    Update madeInheritance.java

--- InheritanceApp/madeInheritance.java
+++ InheritanceApp/madeInheritance.java
@@ -1,9 +1,25 @@
+class cal{
+	int v1,v2;
+	cal(int v1, int v2){
+		this.v1 = v1;
+		this.v2 = v2;	
+	}
+}
+class cal3 extends cal{
+
+	cal3(int v1, int v2) {  // 부모 클래스가 인자를 받는다면, 자식 클래스도 부모가 쓰는 메쏘드를 사용하므로, 
+		super(v1, v2);		// 자식 클래스도 super를 통해 부모의 인자를 받아오는 자신과 같은 이름으로 메쏘드를 만들어 받아야 사용할 수 있다.
+		System.out.println("Cal3 init!!");
+	}
+	
+}
 
 public class madeInheritance {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		cal c = new cal(2,1);
+		cal3 c3 = new cal3(2,1);
+		
 	}
 
 }
